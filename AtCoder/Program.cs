9c500d825c38249ca0b1a6398045cf93284b5b49
Takayuki Sei <takayuki.sei@gree.net>
2019-10-19 22:04:05 +0900

    1

--- AtCoder/Program.cs
+++ AtCoder/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace AtCoder
+﻿using System;
+
+namespace AtCoder
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+            int b = int.Parse(input[0]);
+            int c = int.Parse(input[1]);
+ 
+            string s = Console.ReadLine();
+            Console.WriteLine((a + b + c) + " " + s);
+        }
     }
-  }
 }
